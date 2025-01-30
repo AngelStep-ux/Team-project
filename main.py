b45fe6dc7388ca4076df56019315c538eb4074a3
AngelStep-ux <alina.step@mail.ru>
2025-01-30 13:19:04 +0300

    Update main.py
    добавлены таблицы favorite_users и sessions. Добавлен UNIQUE для user_id в таблице users

--- main.py
+++ main.py
@@ -6,11 +6,30 @@ def create_database():
     
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        user_id INTEGER NOT NULL,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL UNIQUE,
         name TEXT,
         age INTEGER,
         interests TEXT
+    )
+    ''')
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS favorite_users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        favorite_user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        FOREIGN KEY (favorite_user_id) REFERENCES users(user_id)
+    )
+    ''')
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS sessions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        session_start TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
     )
     ''')
     
